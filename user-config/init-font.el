a7cfdeb3e7e0d677ca92ef2fc210e131c7fd3e3f
h5lianfu <h5lianfu@gmail.com>
2024-11-29 03:03:30 -0500

    update little

--- user-config/init-font.el
+++ user-config/init-font.el
@@ -9,21 +9,20 @@
 ;; (set-frame-font "Open Sans-11" t t)
 ;; (set-frame-font "Monaco-11" t t)
 ;; (set-frame-font "Droid Sans Mono-11" t t)
-(set-frame-font "Noto Sans Mono-12" t t)
 ;; (set-frame-font "Ubuntu Mono-13" t t)
-;;(set-face-attribute 'default nil
-;;  :weight 'normal
-;;  :width 'normal)
+(set-frame-font "Noto Sans Mono Regular-14" t t)
 
+;; (set-face-attribute 'default nil :font "Droid Sans Mono")
+;; (set-fontset-font t 'han "PingFang SC Regular")
 
 ;; ------ 设置中文字体 ------
 
-;;(dolist (charset '(kana han symbol cjk-misc bopomofo))
-;;  (if (display-graphic-p)
-;;    (progn
-;;      (set-fontset-font (frame-parameter nil 'font)
-;;        charset
-;;        (font-spec :family "PingFang SC" :size 15)))))
+;; (dolist (charset '(kana han symbol cjk-misc bopomofo))
+;;   (if (display-graphic-p)
+;;     (progn
+;;       (set-fontset-font (frame-parameter nil 'font)
+;;       charset
+;;       (font-spec :family "PingFang SC" :size 14)))))
 
 
 ;; ------ Windows 中文字体兼容 ------
