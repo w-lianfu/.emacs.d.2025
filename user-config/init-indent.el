771531cbf5d9dcee007d9c4cbcb14f7e7d4562a2
h5lianfu <h5lianfu@gmail.com>
2024-11-30 02:38:36 -0500

    update indent

--- user-config/init-indent.el
+++ user-config/init-indent.el
@@ -6,7 +6,20 @@
 
 ;; ------ 设置缩进 ------
 
+;; (electric-indent-mode -1)
+(setq default-tab-width 2)
 (setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq-default web-mode-code-indent-offset 2)
+(setq-default web-mode-css-indent-offset 2)
+(setq-default web-mode-markup-indent-offset 2)
+(setq-default css-indent-offset 2)
+(setq-default sass-indent-offset 2)
+(setq-default js-indent-level 2)
+(setq-default js2-basic-offset 2)
+(setq-default typescript-indent-level 2)
+(setq-default typescript-auto-indent-flag t)
+(setq-default rjsx--indent-running-offset 2)
 
 
 (provide 'init-indent)
